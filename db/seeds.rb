9091a029826dffe61f6739b417d2323b0c5aaa4a
muellerviana <muellerviana@gmail.com>
2022-02-18 08:42:41 -0300

    refact-seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Cadastrando moedas..."
+spinner = TTY::Spinner.new("[:spinner]Cadastrando moedas...")
 
-Coin.create!(
-    description: "Bitcoin",
-    acronym: "BTC",
-    url_image: "https://logosmarcas.net/wp-content/uploads/2020/08/Bitcoin-Emblema.png"
-    
-)
+coins = [{description: "Bitcoin",
+            acronym: "BTC",
+            url_image: "https://logosmarcas.net/wp-content/uploads/2020/08/Bitcoin-Emblema.png"},
 
-Coin.create!(
-    description: "Etherium",
-    acronym: "ETH",
-    url_image: "https://marcas-logos.net/wp-content/uploads/2020/03/ETHEREUM-LOGO.png"
-    
-)
+        {description: "Etherium",
+            acronym: "ETH",
+            url_image: "https://marcas-logos.net/wp-content/uploads/2020/03/ETHEREUM-LOGO.png"},
 
-Coin.create!(
-    description: "Dash",
-    acronym: "DSH",
-    url_image: "https://cryptologos.cc/logos/dash-dash-logo.png"
-    
-)
-puts "Moedas cadastradas com sucesso!"
+        {description: "Dash",
+            acronym: "DSH",
+            url_image: "https://cryptologos.cc/logos/dash-dash-logo.png"},]
+
+coins.each do |coin|
+    Coin.find_or_create_by!(coin)
+end
+
+spinner.success("(Concluído)")
+
+  
